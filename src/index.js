9620966363ac6edc71e49b398965fc5f69b3519a
IrynaRybka <irarybka@gmail.com>
2022-11-10 09:12:26 +0200

    pack to instal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import cardCountry from '../src/template/card-country.hbs';
 
 const DEBOUNCE_DELAY = 300;
+const inputRef = document.querySelector('#search-box');
+const cardInfo = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(writeCountry, DEBOUNCE_DELAY));
+
+function writeCountry(event) {
+  console.log(event.target.value);
+  
+}
+
+// function fetchCountries(name) {
+
+fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
+  .then(responce => {
+   return responce.json()
+  }).then(country => {
+    console.log(country)
+     
+    cardInfo.insertAdjacentHTML("beforeend", cardCountry);
+    }
+)
+  .catch(error => {console.log(error)});
+// }
+
